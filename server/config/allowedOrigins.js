e0567cfe0708e30b6e8da306656c18c2a15bdad7
Amandeep-jadoun <2020kucp1035@iiitkota.ac.in>
2023-08-26 03:32:12 +0530

    add origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,8 +2,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5000",
   "http://localhost:5173",
-  "https://foodiefolio.onrender.com/",
-  "https://amandeep.onrender.com",
+  "https://vercel.com/amandeepjadoun786-gmailcom/foodie-folio",
+  "https://vercel.com/amandeepjadoun786-gmailcom/foodie-folio-backend",
 ];
 
 module.exports = allowedOrigins;
